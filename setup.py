8fe5b5b54f16dcd6d54fb4cfc221365082b3821f
Shahid Khan <47216877+shahidzk1@users.noreply.github.com>
2023-10-13 16:43:16 +0200

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="HP_OPT_class",
+    version="0.1.0",
+    author="Shahid Khan",
+    license="MIT",
+    url="https://github.com/CBM-ML/XGB.git",
+    packages=find_packages()
+)

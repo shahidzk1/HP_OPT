2b1a0781c23d37424a36c3bf8abd66135c087593
Shahid <shahid.khan@uni-tuebingen.de>
2024-01-22 11:04:20 +0100

    changed xgb objective to multi-softproba

--- HP_OPT_class/test_hp_opt_class_code.py
+++ HP_OPT_class/test_hp_opt_class_code.py
@@ -5,11 +5,9 @@ from HP_OPT_class_code import HPOpt
 
 class TestHPOptClass(unittest.TestCase):
     def setUp(self):
-        self.x_train = pd.read_csv('/home/shahid/ML/Machine_learning/Higgs_challenge/testing.csv').iloc[:,0:-1]
-        self.y_train_new = pd.read_csv('/home/shahid/ML/Machine_learning/Higgs_challenge/testing.csv').iloc[:,-1]
-
-    def tearDown(self):
-        pass
+        data = pd.read_csv('/home/shahid/ML/Machine_learning/Higgs_challenge/testing.csv')
+        self.x_train = data.iloc[:, 0:-1]
+        self.y_train_new = data.iloc[:, -1].replace({'s': 1, 'b': 0})
 
     def test_mlp_objective(self):
         optimizer = HPOpt(self.x_train, self.y_train_new)
@@ -20,7 +18,7 @@ class TestHPOptClass(unittest.TestCase):
     def test_xgboost_objective(self):
         y_train_new = self.y_train_new.replace('s', 1, regex=True)
         y_train_new = self.y_train_new.replace('b', 0, regex=True)
-        optimizer = HPOpt(self.x_train, y_train_new, tree_method='auto')
+        optimizer = HPOpt(self.x_train, y_train_new, tree_method_xgb='auto')
         trial = optimizer.optimize("xgboost").trials[0]
         self.assertIn('n_estimators', trial.params)
         self.assertIn('alpha', trial.params)
